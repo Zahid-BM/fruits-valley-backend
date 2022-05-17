3de9adacaf07b00a49a65845338c5f432dac46d3
Zahid-BM <bmzahidulislam@outlook.com>
2022-05-17 17:42:02 +0600

    Alhamdulillah /add api created for userwise email item creation. This is the correct preactice previous was wrong

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ async function run() {
     try {
         await client.connect();
         const itemCollection = client.db('warehouse').collection('items');
+        const addCollection = client.db('warehouse').collection('add');
         const reportCollection = client.db('warehouse').collection('report');
         console.log('db connected');
 
@@ -48,7 +49,6 @@ async function run() {
             });
             res.send({ accessToken });
         });
-
 
         // get all items data from database and send to client side
         app.get('/inventory', async (req, res) => {
@@ -97,9 +97,9 @@ async function run() {
         });
 
         // receive new item add request from client side to save in the data base and then send to client side again
-        app.post('/inventory', async (req, res) => {
+        app.post('/add', async (req, res) => {
             const newItem = req.body;
-            const result = await itemCollection.insertOne(newItem);
+            const result = await addCollection.insertOne(newItem);
             res.send(result);
         });
         // verify JWT and email-wise item find and send to client
@@ -111,60 +111,11 @@ async function run() {
                 const cursor = itemCollection.find(query);
                 const myItems = await cursor.toArray();
                 res.send(myItems);
-                console.log(myItems)
             }
             else {
                 res.status(403).send({ message: 'Forbidden Access' });
             }
         });
-
-
-
-
-
-
-        // old code change
-        // get single service data with id params from database and send to client side
-        app.get('/service/:id', async (req, res) => {
-            const idParams = req.params.id;
-            const query = { _id: ObjectId(idParams) };
-            const service = await serviceCollection.findOne(query);
-            res.send(service);
-        });
-
-
-        // old code change
-        // receive delete request from client side to delete from database 
-        app.delete('/service/:id', async (req, res) => {
-            const idAsParams = req.params.id;
-            const query = { _id: ObjectId(idAsParams) };
-            const result = await serviceCollection.deleteOne(query);
-            res.send(result);
-        });
-
-        // old code change
-        // order collection API
-        app.post('/order', async (req, res) => {
-            const order = req.body;
-            const result = await orderCollection.insertOne(order);
-            res.send(result);
-        });
-
-        // old code change
-        app.get('/orders', verifyJwt, async (req, res) => {
-            const decodedEmail = req.decoded.email;
-            const email = req.query.email;
-            if (email === decodedEmail) {
-                const query = { email };
-                const cursor = orderCollection.find(query);
-                const orders = await cursor.toArray();
-                res.send(orders);
-            }
-            else {
-                res.status(403).send({ message: 'Forbidden Access' });
-            }
-        })
-
     }
     finally {
 
